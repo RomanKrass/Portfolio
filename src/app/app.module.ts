e2a214a4cc5f4d37edfc6ead534f2a1c64b54e9f
Krass Roman <roman.krass@students.bfh.ch>
2023-10-08 11:32:02 +0200

    added footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './Shared/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '../material/material.module';
+import { FooterComponent } from './Shared/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, AboutComponent, HeaderComponent],
+  declarations: [AppComponent, AboutComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
