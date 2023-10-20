d365e45b715652b1701c45d48e296e5107e720a6
RomanKrass <krasr1@bfh.ch>
2023-10-20 22:43:59 +0200

    added first draft for contact site

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  message = '';
 
+  onSubmit() {
+    const email = 'roman.krass@students.bfh.ch';
+    const subject = 'Contact from roman-krass.github.io';
+    const body = `Message: ${this.message}`;
+    window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
+  }
 }
